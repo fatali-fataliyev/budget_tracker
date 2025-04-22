41de1ac3b1c661726ce5bedf8e62dea23926f979
fatali-fataliyev <fatali.fataliiyev@gmail.com>
2025-04-22 21:48:18 +0400

    categories table migration added

---
+++ db/migrations/010_categories_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE categories (
+    id varchar(36) NOT NULL,
+    name varchar(255) NOT NULL,
+    created_at datetime NOT NULL,
+    updated_at datetime NOT NULL,
+    max_amount decimal(20, 2),
+    created_by varchar(36),
+    CONSTRAINT pk_categories PRIMARY KEY(id),
+    CONSTRAINT uq_categories_name UNIQUE(name),
+    CONSTRAINT fk_categories_created_by_users_id FOREIGN KEY(created_by) REFERENCES users(id)
+);
